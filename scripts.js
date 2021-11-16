139d4862fb55c2d221ad8da7abef4c9d86201dc9
Kayla Park <kaylapark@Kaylas-MBP.fios-router.home>
2021-11-16 09:27:42 -0500

    position of text

--- scripts.js
+++ scripts.js
@@ -2,12 +2,12 @@ var leadspace = document.querySelectorAll('.leadspace');
 var text = document.querySelectorAll('.text');
 var svgs = document.querySelectorAll('.leadspace svg');
 var leadspace_width = leadspace[1].offsetWidth;
-var left = leadspace_width/2;
+var left = leadspace_width/2 - 17;
 var previous = 0;
 var index;
 
 svgs.forEach(item => {
-    item.style.left = left.toString() + "px";
+    item.style["margin-left"] = left.toString() + "px";
 });
 
 leadspace.forEach(item => {
