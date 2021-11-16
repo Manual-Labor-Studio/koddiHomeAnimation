d2aa1942d891efb072c2cfaf95c51384e618feac
Kayla Park <kaylapark@Kaylas-MBP.fios-router.home>
2021-11-16 16:33:09 -0500

    hover state

--- scripts.js
+++ scripts.js
@@ -4,31 +4,58 @@ var svgs = document.querySelectorAll('.leadspace svg');
 var leadspace_width = leadspace[1].offsetWidth;
 var left = leadspace_width/2 - 17;
 var previous = 0;
+var width = window.innerWidth;
 var index;
 
-
 function svg_margin() {
+    var margin = 30;
     svgs.forEach(item => {
-        item.style["margin-left"] = left.toString() + "px";
+        if(width>768) {
+            item.style["margin-left"] = left.toString() + "px";
+        }
+        else {
+            item.style["margin-left"] = margin.toString() + "px";
+            margin += 32;
+        }
     });
 }
 
-leadspace.forEach(item => {
-    item.addEventListener('click', function(){
-        index = [].indexOf.call(leadspace, item);
-        if(index != previous) {
-            item.classList.add("expand");
-            leadspace[previous].classList.remove("expand");
-            text[previous].classList.add("hide");
-            setTimeout(function(){
-                text[index].classList.remove("hide");
-            },800);
-            previous = index;
-        }
+function hide_text() {
+    text[previous].classList.add("hide");
+    setTimeout(function(){
+        text[index].classList.remove("hide");
+    },800);
+}
+
+if(width>768) {
+    leadspace.forEach(item => {
+        item.addEventListener('click', function(){
+            index = [].indexOf.call(leadspace, item);
+            if(index != previous) {
+                item.classList.add("expand");
+                leadspace[previous].classList.remove("expand");
+                svgs[previous].classList.remove("clicked");
+                svgs[index].classList.add("clicked");
+                hide_text();
+                previous = index;
+            }
+        });
     });
-});
+}
+
+else {
+    svgs.forEach(item => {
+        item.addEventListener('click', function(){
+            index = [].indexOf.call(svgs, item);
+            leadspace[previous].classList.add("hide");
+            hide_text();
+            previous = index;
+        });
+    });
+}
 
 function reportWindowSize() {
+    width = window.innerWidth;
     var idx;
     for(var x=0; x<3; x++) {
         if(x!=previous && x!=index) {
@@ -41,4 +68,4 @@ function reportWindowSize() {
 }
   
 window.onresize = reportWindowSize;
-window.onload = svg_margin;
+window.onload = reportWindowSize;
